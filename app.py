bc17c020bb55f8b918248927d9b2b9e1100b09e2
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-03-18 21:36:34 -0600

    rework hems data & move scrape data to app.py

--- app.py
+++ app.py
@@ -6,3 +6,38 @@ app=Flask(__name__)
 mongo_conn='mongodb://localhost:27017'
 client=pymongo.MongoClient(mongo_conn)
 
+@app.route('/')
+def index():
+	data_from_mongo=client.mars_db_one.mars_info.find_one()
+	#print(list(data_from_mongo))	
+	if data_from_mongo:
+		return render_template('index.html', data_from_flask=data_from_mongo)
+	else:
+		return 'Error Try Again'
+	#return render_template('index.html', dict=data_from_mongo)
+
+create a new route:
+@app.route('/scrape')
+def scrape():
+	mars_data= scrape_mars.scrape()
+	print(mars_data)
+	client.mars_db_one.mars_info.update({}, mars_data, upsert=True)
+	
+
+# 	print ('clicked button') #it needs to call the scrape_mars.py. 
+
+# 	what is important is that it will return a dict of results
+# 	# store the dict of results to mongo using client.mars_db_one.mars_info.insert(, upsert=True)
+#	mars_info=
+
+# 	data_from_mongo=client.mars_db_one.find_one()
+
+# 	return render_template('index.html', data_from_flask=data_from)
+# 	call the scrape_mars.py, which will retrun a dict of ....
+
+# 	store the dict of results to mongodb 
+# 		mars_db_one -- collection mars_info # need to add image & hemisphere images/hrefs still
+	return redirect('/')
+
+if __name__=='__main__':
+	app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,38 +1,8 @@
-from flask import Flask, render_template, redirect
+from bs4 import BeautifulSoup
+from splinter import Browser
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 import pymongo
-
-app=Flask(__name__)
-
-mongo_conn='mongodb://localhost:27017'
-client=pymongo.MongoClient(mongo_conn)
-
-
-@app.route('/')
-def index():
-	data_from_mongo=client.mars_db_one.mars_info.find_one()
-	#print(list(data_from_mongo))	
-	return render_template('index.html', data_from_flask=data_from_mongo)
-	#return render_template('index.html', dict=data_from_mongo)
-
-# create a new route:
-# @app.route('/scrape')
-# def scrape():
-# 	print ('clicked button') #it needs to call the scrape_mars.py. 
-
-# 	what is important is that it will return a dict of results
-# 	# store the dict of results to mongo using client.mars_db_one.mars_info.insert(, upsert=True)
-#	mars_info=
-
-# 	data_from_mongo=client.mars_db_one.find_one()
-
-# 	return render_template('index.html', data_from_flask=data_from)
-# 	call the scrape_mars.py, which will retrun a dict of ....
-
-# 	store the dict of results to mongodb 
-# 		mars_db_one -- collection mars_info # need to add image & hemisphere images/hrefs still
-	return redirect('/')
-
-if __name__=='__main__':
-	app.run(debug=True)
-
-
+from pymongo import MongoClient
+import pandas as pd
